5e2299c0ee809c9743b7c3d75a9f9899cf1aae8a
christianfirecup <christianfirecup@aol.com>
2022-08-10 04:38:59 -0400

    item test complete

--- src/main/java/firecup/mods/viewmods/Viewmods.java
+++ src/main/java/firecup/mods/viewmods/Viewmods.java
@@ -1,10 +1,17 @@
 package firecup.mods.viewmods;
 
+import firecup.mods.viewmods.item.ViewerItems;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Viewmods implements ModInitializer {
+    public static final String MOD_ID = "viewmods";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
+        ViewerItems.registermoditems();
 
     }
 }
--- src/main/java/firecup/mods/viewmods/item/ViewerItems.java
+++ src/main/java/firecup/mods/viewmods/item/ViewerItems.java
@@ -1,4 +1,22 @@
 package firecup.mods.viewmods.item;
 
+import firecup.mods.viewmods.Viewmods;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class ViewerItems {
+    public static final Item STONESTICK = registeritem("stone_stick" , new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+    private static Item registeritem(String name, Item item){
+        return Registry.register(Registry.ITEM,new Identifier(Viewmods.MOD_ID, name), item);
+    }
+    public static void registermoditems(){
+        Viewmods.LOGGER.debug("Reg items for " + Viewmods.MOD_ID);
+    }
+
 }
